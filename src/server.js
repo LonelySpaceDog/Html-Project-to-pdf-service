de18fc536ca86f4451fc5bc2e9c95291023fef3c
Viacheslav Vasiliev <megbegbonok@gmail.com>
2021-04-01 21:35:32 +0500

    fix mkdir unzipped and patches postinstall

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ dotenv.config({ path: `${__dirname}/../config.env` });
 const app = require(`${__dirname}/app`);
 const { loggerApp } = require(`${__dirname}/utils/logger`);
 const port = process.env.PORT;
+const { mkdir } = require('fs');
+
+mkdir(`${__dirname}/../unzipped/`, { recursive: true }, (err) => {
+  if (err) {
+    loggerApp.error(err);
+    throw err;
+  }
+});
 
 app.listen(port, () => {
   loggerApp.info(`listening on port ${port}`);
