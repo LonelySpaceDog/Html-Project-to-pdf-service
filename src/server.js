8537a26522b8f1356eaf788a6392ca3b1ef3e063
Viacheslav Vasiliev <megbegbonok@gmail.com>
2021-04-01 22:01:12 +0500

    mkdir pdfFiles

--- src/server.js
+++ src/server.js
@@ -5,6 +5,13 @@ const { loggerApp } = require(`${__dirname}/utils/logger`);
 const port = process.env.PORT;
 const { mkdir } = require('fs');
 
+mkdir(`${__dirname}/../pdfFiles/`, { recursive: true }, (err) => {
+  if (err) {
+    loggerApp.error(err);
+    throw err;
+  }
+});
+
 mkdir(`${__dirname}/../unzipped/`, { recursive: true }, (err) => {
   if (err) {
     loggerApp.error(err);
